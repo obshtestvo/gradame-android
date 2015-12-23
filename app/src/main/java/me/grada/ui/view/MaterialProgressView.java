dc63bbcf859e7e268dead70d1ee899a1c204707a
yavorivanov <mobilekid7@googlemail.com>
2015-12-23 14:23:06 +0000

    Port of the Material Design progress view to pre-Android Lollipop. Added animations FAB-like animation for views.

---
+++ app/src/main/java/me/grada/ui/view/MaterialProgressView.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Obshtestvo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.grada.ui.view;
+
+/**
+ * Created by yavorivanov on 23/12/2015.
+ */
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import me.grada.R;
+import me.grada.utils.ViewUtils;
+
+/**
+ * Created by yavorivanov on 23/12/2015.
+ */
+public class MaterialProgressView extends ImageView {
+
+    private static final int KEY_SHADOW_COLOR = 0x1E000000;
+    private static final int FILL_SHADOW_COLOR = 0x3D000000;
+    // PX
+    private static final float X_OFFSET = 0f;
+    private static final float Y_OFFSET = 1.75f;
+    private static final float SHADOW_RADIUS = 3.5f;
+    private static final int SHADOW_ELEVATION = 4;
+
+    private static final int CIRCLE_DIAMETER = 40;
+
+    private Animation.AnimationListener mListener;
+    private int mShadowRadius;
+
+    private MaterialProgressDrawable materialProgressDrawable;
+
+    public MaterialProgressView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public MaterialProgressView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+        final float density = context.getResources().getDisplayMetrics().density;
+        final int diameter = (int) (CIRCLE_DIAMETER * density * 2);
+        final int shadowYOffset = (int) (density * Y_OFFSET);
+        final int shadowXOffset = (int) (density * X_OFFSET);
+
+        mShadowRadius = (int) (density * SHADOW_RADIUS);
+
+        ShapeDrawable circle;
+        if (elevationSupported()) {
+            circle = new ShapeDrawable(new OvalShape());
+            ViewCompat.setElevation(this, SHADOW_ELEVATION * density);
+        } else {
+            OvalShape oval = new OvalShadow(mShadowRadius, diameter);
+            circle = new ShapeDrawable(oval);
+            ViewCompat.setLayerType(this, ViewCompat.LAYER_TYPE_SOFTWARE, circle.getPaint());
+            circle.getPaint().setShadowLayer(mShadowRadius, shadowXOffset, shadowYOffset,
+                    KEY_SHADOW_COLOR);
+            final int padding = mShadowRadius;
+            // set padding so the inner image sits correctly within the shadow.
+            setPadding(padding, padding, padding, padding);
+        }
+        circle.getPaint().setColor(ViewUtils.getColor(getResources(), android.R.color.white));
+        ViewUtils.setBackground(this, circle);
+
+        // Set the ring as image
+        materialProgressDrawable = new MaterialProgressDrawable(context, this);
+        materialProgressDrawable.setColorSchemeColors(ViewUtils.getColor(getResources(), R.color.orange_600));
+        materialProgressDrawable.showArrow(true);
+        materialProgressDrawable.setAlpha(255);
+        setImageDrawable(materialProgressDrawable);
+
+        // Start the animation explicitly if the view was only defined in layout and was visible
+        setVisibility(getVisibility());
+    }
+
+
+    private boolean elevationSupported() {
+        return android.os.Build.VERSION.SDK_INT >= 21;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (!elevationSupported()) {
+            setMeasuredDimension(getMeasuredWidth() + mShadowRadius*2, getMeasuredHeight()
+                    + mShadowRadius*2);
+        }
+    }
+
+    public void setAnimationListener(Animation.AnimationListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    public void onAnimationStart() {
+        super.onAnimationStart();
+        if (mListener != null) {
+            mListener.onAnimationStart(getAnimation());
+        }
+    }
+
+    @Override
+    public void onAnimationEnd() {
+        super.onAnimationEnd();
+        if (mListener != null) {
+            mListener.onAnimationEnd(getAnimation());
+        }
+    }
+
+    /**
+     * Update the background color of the circle image view.
+     *
+     * @param colorRes Id of a color resource.
+     */
+    public void setBackgroundColorRes(int colorRes) {
+        setBackgroundColor(getContext().getResources().getColor(colorRes));
+    }
+
+    @Override
+    public void setBackgroundColor(int color) {
+        if (getBackground() instanceof ShapeDrawable) {
+            ((ShapeDrawable) getBackground()).getPaint().setColor(color);
+        }
+    }
+
+    @Override
+    public void setVisibility(int visibility) {
+        if (visibility == View.VISIBLE) {
+            materialProgressDrawable.start();
+        } else {
+            materialProgressDrawable.stop();
+        }
+        super.setVisibility(visibility);
+    }
+
+    private class OvalShadow extends OvalShape {
+        private RadialGradient mRadialGradient;
+        private Paint mShadowPaint;
+        private int mCircleDiameter;
+
+        public OvalShadow(int shadowRadius, int circleDiameter) {
+            super();
+            mShadowPaint = new Paint();
+            mShadowRadius = shadowRadius;
+            mCircleDiameter = circleDiameter;
+            mRadialGradient = new RadialGradient(mCircleDiameter / 2, mCircleDiameter / 2,
+                    mShadowRadius, new int[] {
+                    FILL_SHADOW_COLOR, Color.TRANSPARENT
+            }, null, Shader.TileMode.CLAMP);
+            mShadowPaint.setShader(mRadialGradient);
+        }
+
+        @Override
+        public void draw(Canvas canvas, Paint paint) {
+            final int viewWidth = MaterialProgressView.this.getWidth();
+            final int viewHeight = MaterialProgressView.this.getHeight();
+            canvas.drawCircle(viewWidth / 2, viewHeight / 2, (mCircleDiameter / 2 + mShadowRadius),
+                    mShadowPaint);
+            canvas.drawCircle(viewWidth / 2, viewHeight / 2, (mCircleDiameter / 2), paint);
+        }
+    }
+}
---
+++ app/src/main/java/me/grada/utils/ViewUtils.java
@@ -0,0 +1,126 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Obshtestvo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.grada.utils;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.view.View;
+import android.view.animation.Interpolator;
+
+/**
+ * Created by yavorivanov on 23/12/2015.
+ */
+public class ViewUtils {
+
+    public static final Interpolator FAST_OUT_SLOW_IN_INTERPOLATOR = new FastOutSlowInInterpolator();
+
+    private ViewUtils() {}
+
+    /**
+     * Resolves a color resource taking into account {@link android.os.Build.VERSION#SDK_INT}.
+     *
+     * @param resources Activity's resources.
+     * @param color     Color's resource id.
+     * @return Color's hex.
+     */
+    public static int getColor(Resources resources, int color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return resources.getColor(color, null);
+        } else {
+            return resources.getColor(color);
+        }
+    }
+
+    /**
+     * Sets a drawable as background taking into account {@link android.os.Build.VERSION#SDK_INT}.
+     *
+     * @param view The view to receive a new background.
+     * @param drawable The drawable to be set as background.
+     */
+    public static void setBackground(View view, Drawable drawable) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            view.setBackground(drawable);
+        } else {
+            view.setBackgroundDrawable(drawable);
+        }
+    }
+
+    /**
+     * Animates in the passed in view by scaling its x and y in to 1.0f and setting its alpha to 1.0f.
+     * @param view The view to receive the animation.
+     */
+    public static void animateIn(final View view) {
+        if (view.getVisibility() != View.VISIBLE) {
+            if (ViewCompat.isLaidOut(view) && !view.isInEditMode()) {
+                view.setScaleX(0.0f);
+                view.setScaleY(0.0f);
+                view.setAlpha(0.0f);
+                view.animate()
+                        .scaleX(1.0F)
+                        .scaleY(1.0F)
+                        .alpha(1.0F)
+                        .setDuration(200L)
+                        .setInterpolator(FAST_OUT_SLOW_IN_INTERPOLATOR)
+                        .setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationStart(Animator animation) {
+                                view.setVisibility(View.VISIBLE);
+                            }
+                        });
+            }
+        }
+    }
+
+    /**
+     * Animates out the passed in view by scaling its x and y out to .0f and setting its alpha to .0f.
+     * @param view The view to receive the animation.
+     */
+    public static void animateOut(final View view) {
+        if (view.getVisibility() == View.VISIBLE) {
+            if (ViewCompat.isLaidOut(view) && !view.isInEditMode()) {
+                view.animate()
+                        .scaleX(0.0F)
+                        .scaleY(0.0F)
+                        .alpha(0.0F)
+                        .setDuration(200L)
+                        .setInterpolator(FAST_OUT_SLOW_IN_INTERPOLATOR)
+                        .setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                view.setVisibility(View.GONE);
+                            }
+                        });
+            } else {
+                view.setVisibility(View.GONE);
+            }
+        }
+    }
+
+}
