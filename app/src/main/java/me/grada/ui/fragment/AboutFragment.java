ca883c7711c9388529c54f0ac129caaa865e63f0
yavorivanov <mobilekid7@googlemail.com>
2015-12-28 20:41:52 +0000

    Listed 3rd party libs' licenses in About.

--- app/src/main/java/me/grada/ui/fragment/AboutFragment.java
+++ app/src/main/java/me/grada/ui/fragment/AboutFragment.java
@@ -24,17 +24,35 @@
 
 package me.grada.ui.fragment;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Browser;
+import android.support.annotation.Nullable;
+import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.URLSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import butterknife.Bind;
 import me.grada.R;
 
 /**
  * Created by yavorivanov on 28/12/2015.
  */
 public class AboutFragment extends BaseFragment {
+
+    @Bind(R.id.about_the_app)
+    TextView aboutView;
+
+    @Bind(R.id.third_party_licenses)
+    TextView thirdPartyLicencesView;
 
     public static AboutFragment newInstance() {
         return new AboutFragment();
@@ -46,4 +64,36 @@ public class AboutFragment extends BaseFragment {
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        aboutView.setText(getString(R.string.about_me_notice, getString(R.string.app_name)));
+
+        CharSequence sequence = Html.fromHtml(getString(R.string.third_party_licenses));
+        SpannableStringBuilder strBuilder = new SpannableStringBuilder(sequence);
+        URLSpan[] urls = strBuilder.getSpans(0, sequence.length(), URLSpan.class);
+        for (URLSpan span : urls) {
+            makeLinkClickable(strBuilder, span);
+        }
+        thirdPartyLicencesView.setMovementMethod(LinkMovementMethod.getInstance());
+        thirdPartyLicencesView.setText(strBuilder);
+    }
+
+    private void makeLinkClickable(SpannableStringBuilder strBuilder, final URLSpan span) {
+        int start = strBuilder.getSpanStart(span);
+        int end = strBuilder.getSpanEnd(span);
+        int flags = strBuilder.getSpanFlags(span);
+        ClickableSpan clickable = new ClickableSpan() {
+            public void onClick(View view) {
+                Uri uri = Uri.parse(span.getURL());
+                Context context = view.getContext();
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                intent.putExtra(Browser.EXTRA_APPLICATION_ID, context.getPackageName());
+                context.startActivity(intent);
+            }
+        };
+        strBuilder.setSpan(clickable, start, end, flags);
+        strBuilder.removeSpan(span);
+    }
 }
